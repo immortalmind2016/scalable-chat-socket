f27b5163bf6d91a1fc7a3bbc952527a838daf1d1
mohamed salah <immortal.mind2016@gmail.com>
2021-10-21 14:59:17 +0200

    init

---
+++ servers/server1/index.ts
@@ -0,0 +1,54 @@
+
+import {createClient} from "redis"
+
+
+const publisher = createClient({
+    url:"redis://localhost:6379"
+});
+const subscriber = createClient({
+    url:"redis://localhost:6379"
+});
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors:{
+      origin:"*"
+  }
+});
+const rooms={
+    room1:["user20","user21"]
+}
+
+io.on("connection", (socket: Socket) => {
+console.log("connection")
+socket.on("join",({room,user})=>{
+  try{
+    console.log("JOIN")
+    rooms[room].push(user)
+    console.log({rooms})
+    socket.join(room)
+
+    io.to(room).emit("join",user)
+    publisher.publish("join-from-1-to-2", JSON.stringify({room,user}));
+  }catch(e){
+      console.log(e)
+  }
+})
+});
+subscriber.on('message',(channel,message)=>{
+    console.log({message,channel})
+    if(channel=="join-from-2-to-1")
+   {
+    const {room,user}=JSON.parse(message)
+    console.log("SUBSCRIP ",room,user)
+    io.to(room).emit("join",user)
+   }
+})
+
+httpServer.listen(3000)
+subscriber.subscribe("join-from-2-to-1")
+
+
+
